220c42ef730cc35a03353253afab58571e584934
romanV7 <51646341+romanV7@users.noreply.github.com>
2020-05-28 03:45:50 +0300

    fix cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const Controller = require('./api/controllers/controller')
 
 const app = (request, response) => {
+  response.setHeader('Content-Type', 'application/json')
+  response.setHeader('Access-Control-Allow-Origin', '*')
   const { headers, method, url } = request
   if (url === '/courses/getAllCourses') return Controller.getAllCourses(request, response)
   if (url.includes('/courses/getPair')) return Controller.getUniquePair(request, response)
